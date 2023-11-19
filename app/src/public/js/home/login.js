203a5eb71fd98c9fa22961f29fa72d1699d575a9
mj <mj577900@gmail.com>
2023-11-19 15:11:58 +0900

    add DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log('hello');
-console.log('bye');
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value
+    };
+    console.log(req);
+}
