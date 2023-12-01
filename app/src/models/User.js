efe0d039a3215c6e87af37411d71101a71b8ad5d
mj <mj577900@gmail.com>
2023-12-01 13:30:19 +0900

    AWS RDS & dotenv

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,19 @@ class User {
 
   async login() {
     const client = this.body;
-    const {id, password} = await UserStorage.getUserInfo(client.id);
-
-    if (id) {
-      if (id === client.id && password === client.password) {
-        return { success: true };
+    try {
+      const {id, password} = await UserStorage.getUserInfo(client.id);
+  
+      if (id) {
+        if (id === client.id && password === client.password) {
+          return { success: true };
+        }
+        return { success: false, message: '비밀번호가 틀렸어' };
       }
-      return { success: false, message: '비밀번호가 틀렸어' };
+      return { success: false, message: '존재하지 않는 아이디야' };
+    } catch (err) {
+      return { success: false, message: err };
     }
-    return { success: false, message: '존재하지 않는 아이디야' };
   }
 
   async register() {
